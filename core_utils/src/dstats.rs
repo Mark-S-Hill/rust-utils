2b4afa6a67f1dca469919a41385c2f31fffca778
Mark-S-Hill <76998971+Mark-S-Hill@users.noreply.github.com>
2021-09-07 12:05:09 +0100

    refactor dstats

--- core_utils/src/dstats.rs
+++ core_utils/src/dstats.rs
@@ -3,33 +3,45 @@ use fs_extra::dir::get_size;
 use pretty_bytes::converter::convert;
 use jwalk::{WalkDir, Parallelism};
 use rayon::prelude::*;
+use rayon::iter::ParallelBridge;
+
+fn count_objects(obj: &str, path: &str, threads: usize) -> usize {
+    
+    let file_dirs = WalkDir::new(path)
+                    .parallelism(Parallelism::RayonNewPool(threads))
+                    .into_iter()
+                    .par_bridge()
+                    .filter_map(Result::ok);
+
+    if obj == "file" {    
+        
+        file_dirs
+        .filter(|entry| entry.path().is_file())
+        .count()    
+    
+    } else if obj == "directory" {
+        
+        file_dirs
+        .filter(|entry| entry.path().is_dir())
+        .count()      
+    
+    } else {
+        0
+    }
+    
+}
 
 fn main() {
     let args: Vec<String> = env::args().collect();
     let threads: usize = args[1].parse::<usize>().unwrap();
     let path = args[2].to_string();                         
     
-    let dir_size = get_size(&path).unwrap() as f64;
-    
-    let sub_dirs = WalkDir::new(&path)
-                     .parallelism(Parallelism::RayonNewPool(threads))
-                     .into_iter()
-                     .par_bridge()
-                     .filter_map(Result::ok)
-                     .filter(|entry| entry.path().is_dir())
-                     .count();
-
-    let files = WalkDir::new(&path)
-                    .parallelism(Parallelism::RayonNewPool(threads))
-                    .into_iter()
-                    .par_bridge()
-                    .filter_map(Result::ok)
-                    .filter(|entry| entry.path().is_file())
-                    .count();    
+    let dir_size = get_size(&path).unwrap() as f64;    
+    let files = count_objects("file", &path, threads);
+    let sub_dirs = count_objects("directory", &path, threads); 
                                                               
     println!("Parent-directory: {}\tSub-directories: {}\tTotal-files: {}", 
-             convert(dir_size), 
-             sub_dirs,
+             convert(dir_size),
+             sub_dirs, 
              files);
 }
-
