1f542643574eae7cb6363276b4c4c38e6b4300af
Mark-S-Hill <76998971+Mark-S-Hill@users.noreply.github.com>
2021-09-08 16:55:21 +0100

    add error grab gpwd

--- nextflow_utils/src/gpwd.rs
+++ nextflow_utils/src/gpwd.rs
@@ -6,6 +6,7 @@ struct Cli {
     pattern: String,
     #[structopt(parse(from_os_str))]
     path: std::path::PathBuf,
+    error: String,
 }
 
 fn main() {
@@ -16,7 +17,9 @@ fn main() {
     let re = Regex::new(r"[a-z0-9]{2}/[a-z0-9]{6}").unwrap();    
     let cached = "Cached process";
     let submitted = "Submitted process";
-    //let complete = "Task completed";
+    let complete = "Task completed";
+    //let no_error = "exit: 0;";
+    let error = Regex::new(r"exit: [1-9][0-9]*").unwrap();
             
     for line in content.lines() {
         if line.contains(&args.pattern) && 
@@ -40,16 +43,28 @@ fn main() {
                             line.split(" ").nth(12).unwrap(),
                             line.split(" ").nth(13).unwrap())
                             
-                } /* else if line.contains(complete) {
+                } else if line.contains(complete) {
                    
+                   if &args.error == "TRUE" &&
+                        error.is_match(line) {
+            
+                       println!("Process: {}{} {} {} WorkDir:{}", 
+                                line.split(" ").nth(15).unwrap(),
+                                line.split(" ").nth(16).unwrap(),
+                                line.split(" ").nth(19).unwrap(),
+                                line.split(" ").nth(20).unwrap(),
+                                line.split(" ").nth(24).unwrap()   
+                                );
+                   }
+                   /*
                     println!("Workdir: {}\t{} {}: {} {}", 
                             line.split(" ").nth(24).unwrap(),
                             line.split(" ").nth(7).unwrap(),
                             line.split(" ").nth(8).unwrap(),
                             line.split(" ").nth(15).unwrap(),
                             line.split(" ").nth(16).unwrap()) 
-                     
-                } */   
+                    */ 
+                }    
         }
     }
 }
